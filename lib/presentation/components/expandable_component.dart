0a85256344e99f069872d4527a0a57feb596990e
Vikranth <vikkkysalian@gmail.com>
2023-06-14 14:08:30 +0530

    updates

--- lib/presentation/components/expandable_component.dart
+++ lib/presentation/components/expandable_component.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:travelgrid/presentation/widgets/icon.dart';
-import 'package:travelgrid/presentation/widgets/switch.dart';
 import 'package:travelgrid/presentation/widgets/text_view.dart';
 
 class ExpandableComponent extends StatefulWidget {
